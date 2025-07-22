397306f4334628f185e13fc54641c3d79bc71064
Rycherd Dheurymy da Silva <dheurymy@outlook.com>
2025-07-22 04:08:20 -0300

    add Home.js and home.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,61 @@
-import React from 'react'
 
-const Home = () => {
+
+import '../assets/styles/home.css';
+import Logo from '../assets/images/logo-marago-branco.jpg';
+import { useState } from 'react';
+
+const LandingPage = () => {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(prev => !prev);
+  };
   return (
-    <div>
-      <h1>Bem-vindo à plataforma MaraGO!</h1>
-      <p>Aqui você pode encontrar uma variedade de recursos e ferramentas para ajudá-lo em sua jornada.</p>
-      <button onClick={() => window.location.href = '/cadastro'}>Cadastre-se</button>
-      <button onClick={() => window.location.href = '/login'}>Entrar</button>
-      <p>Explore nossas funcionalidades e aproveite ao máximo sua experiência!</p>
-    </div>
-  )
-}
+    <div className="landing-page">
+      <div className='header'>
+        {/* Botão de Menu */}
+        <div className='header-menu'>
+          <label className="hamburger">
+            <input type="checkbox" checked={menuAberto} onChange={toggleMenu} />
+            <svg viewBox="0 0 32 32">
+              <path className="line line-top-bottom" d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"></path>
+              <path className="line" d="M7 16 27 16"></path>
+            </svg>
+          </label>
+        </div>
 
-export default Home
+        {/* Logo */}
+        <div className='content'>
+          <div className='content-logo'>
+            <img src={Logo} alt="Logo MaraGO" />
+          </div>
+          <button onClick={() => window.location.href = '/login'}>Entrar</button>
+        </div>
+
+        {/* Menu Dinâmico */}
+        {menuAberto && (
+          <div className='menu'>
+            <ul>
+
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <main className='main-content'>
+        <h1>
+          <span className='linha1'>O que você</span>
+          <span className='linha2'>quer <strong>fazer</strong></span>
+          <span className='linha3'>hoje?</span>
+        </h1>
+        <p>Com o MaraGO você encontra rolês incríveis perto de você. Explore as opções disponíveis, escolha os que dão match com você, tenha um mapa personalizado com seus rolês favoritos, ganhe insignias por conhecer novos lugares, compartilhe suas experiências e conecte-se com outros usuários!</p>
+        <button onClick={() => window.location.href = '/cadastro'}>Cadastre-se</button>
+      </main>
+
+
+
+    </div>
+  );
+};
+
+export default LandingPage;
